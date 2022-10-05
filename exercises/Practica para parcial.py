a9e5e248ea96d5e40b86c0b46eec4cdea420269c
Matias Ruiz <matiasruiz94m@gmail.com>
2022-10-05 12:51:56 -0300

    Practica Pre parcial completa (ej 3 solo)

--- exercises/Practica para parcial.py
+++ exercises/Practica para parcial.py
@@ -7,24 +7,33 @@ class Vehiculo:
         print( f"Patente:{self.patente} {self.descripcion}" )
 
     def validarPatente(self):
-        if len(self.patente) != 6 or len(self.patente) != 8:
-            print('Patente invalida')
+        if len(self.patente) != 6 and len(self.patente) != 8:
+            return False
         else:
-            print('Patente valida')
+            return True
 
 asd = Vehiculo('123asd')
-asd.validarPatente()
-asd.mostrar()
 
-class Estadia(Vehiculo):
-    def __init__(self, entrada, salida):
+class Estadia:
+    tarifa = 15
+    def __init__(self, entrada, salida, Vehiculo):
         self.entrada = entrada
         self.salida = salida
+        self.Vehiculo = Vehiculo
     
     def mostrar(self):
-        Vehiculo.mostrar
+        self.Vehiculo.mostrar()
         print( f'Hora de entrada:{self.entrada}, Hora de salida:{self.salida}')
+    
+    def calcularTarifaTotal(self):
+        if self.Vehiculo.validarPatente() == False or self.salida - self.entrada < 0:
+            return print('Error no puede estar en esta estadia')
+        
+        return print ((self.salida - self.entrada) * self.tarifa)
+        
 
 
-asd2 = Vehiculo('123asd', 'desc', 13, 15)
+asd2 = Estadia(16, 15, asd)
+asd2.mostrar()
+asd2.calcularTarifaTotal()
 
