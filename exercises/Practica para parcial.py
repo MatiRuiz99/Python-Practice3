343a3b97bc9c01d672fe6ba38ba7c827a38dde85
Matias Ruiz <matiasruiz94m@gmail.com>
2022-10-05 12:23:38 -0300

    jasd

--- exercises/Practica para parcial.py
+++ exercises/Practica para parcial.py
@@ -1,16 +1,30 @@
-from pydoc import describe
-
-
-class Vehiculo():
-    def __init__ (self,patente, descripcion=''):
+class Vehiculo:
+    def __init__ (self,patente:str, descripcion=''):
         self.patente = patente
         self.descripcion = descripcion
     
     def mostrar(self):
-        def __str__ (self):
-        return f"Patente: {"
+        print( f"Patente:{self.patente} {self.descripcion}" )
+
+    def validarPatente(self):
+        if len(self.patente) != 6 or len(self.patente) != 8:
+            print('Patente invalida')
+        else:
+            print('Patente valida')
+
+asd = Vehiculo('123asd')
+asd.validarPatente()
+asd.mostrar()
+
+class Estadia(Vehiculo):
+    def __init__(self, entrada, salida):
+        self.entrada = entrada
+        self.salida = salida
+    
+    def mostrar(self):
+        Vehiculo.mostrar
+        print( f'Hora de entrada:{self.entrada}, Hora de salida:{self.salida}')
 
 
-
-asd = Vehiculo(123, 'asdnafla')
+asd2 = Vehiculo('123asd', 'desc', 13, 15)
 
